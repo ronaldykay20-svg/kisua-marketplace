fbfddc809a1d7799bdff0fd384464fc3f344f6c5
ronaldykay20-svg <ronaldykay20@gmail.com>
2026-05-05 09:45:12 +0100

    Remove NewNavbar and BottomNav components

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,3 @@
-import NewNavbar from "@/components/NewNavbar";
-import BottomNav from "@/components/BottomNav";
 import HomeBannerSlot from "@/components/HomeBannerSlot";
 import FeaturedSellers from "@/components/FeaturedSellers";
 import PromoProductCards from "@/components/PromoProductCards";
@@ -9,29 +7,18 @@ import Footer from "@/components/Footer";
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-background pb-14 md:pb-0">
-      <NewNavbar />
-
+    <>
       <HomeBannerSlot slot={1} />
       <HomeBannerSlot slot={2} />
       <HomeBannerSlot slot={3} />
       <HomeBannerSlot slot={4} />
       <HomeBannerSlot slot={5} />
-
-      {/* 6. Vendedores em destaque com capa + 3 produtos */}
       <FeaturedSellers />
-
       <HomeBannerSlot slot={6} />
       <HomeBannerSlot slot={7} />
-
-      {/* 9. Cards de promoção */}
       <PromoProductCards />
-
       <HomeBannerSlot slot={8} />
-
-      {/* 11. Stories de vídeo */}
       <GroupedVideoStories />
-
       <HomeBannerSlot slot={9} />
       <HomeBannerSlot slot={10} />
       <HomeBannerSlot slot={11} />
@@ -40,13 +27,9 @@ const Index = () => {
       <HomeBannerSlot slot={14} />
       <HomeBannerSlot slot={15} />
       <HomeBannerSlot slot={16} />
-
-      {/* 20. Scroll infinito de produtos */}
       <InfiniteProducts />
-
       <Footer />
-      <BottomNav />
-    </div>
+    </>
   );
 };
 
