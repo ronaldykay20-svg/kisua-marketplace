7921522cba65f5c2d1087dbdd9c88394ab922bb4
ronaldykay20-svg <ronaldykay20@gmail.com>
2026-06-21 14:21:18 +0100

    Add routes for EntregaFrete and Devolucoes pages

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,8 @@ import Favoritos from "./pages/Favoritos.tsx";
 import Ajuda from "./pages/Ajuda.tsx";
 import ComoComprar from "./pages/ComoComprar.tsx";
 import FormasPagamento from "./pages/FormasPagamento.tsx";
+import EntregaFrete from "./pages/EntregaFrete.tsx";
+import Devolucoes from "./pages/Devolucoes.tsx";
 import VenderKwanza from "./pages/VenderKwanza.tsx";
 import Auth from "./pages/Auth.tsx";
 import AdminPanel from "./pages/AdminPanel.tsx";
@@ -102,6 +104,8 @@ const App = () => (
             <Route path="/ajuda" element={<Layout><Ajuda /></Layout>} />
             <Route path="/como-comprar" element={<Layout><ComoComprar /></Layout>} />
             <Route path="/formas-pagamento" element={<Layout><FormasPagamento /></Layout>} />
+            <Route path="/entrega-frete" element={<Layout><EntregaFrete /></Layout>} />
+            <Route path="/devolucoes" element={<Layout><Devolucoes /></Layout>} />
             <Route path="/vender" element={<Layout><ProtectedRoute><VenderKwanza /></ProtectedRoute></Layout>} />
             <Route path="/enderecos" element={<Layout><ProtectedRoute><Enderecos /></ProtectedRoute></Layout>} />
             <Route path="/pagamentos" element={<Layout><ProtectedRoute><Pagamentos /></ProtectedRoute></Layout>} />
